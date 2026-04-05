3e1bcb42bb34490bd21fe33d7609409517601ecd
Jason Sadiki <jason@thilisadiki.dev>
2026-04-05 10:08:31 +0200

    feat: add redirects for articles to improve navigation and resource access

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/articles',
+        destination: 'https://articles.graduateshub.co.za',
+        permanent: true,
+      },
+    ];
+  },
   devIndicators: false,
   images: {
     remotePatterns: [
