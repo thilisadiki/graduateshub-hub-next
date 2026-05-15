dd5f7b3056f8bfbd07ca03b5d5e62d9f502ca254
Jason Sadiki <sadikithili@gmail.com>
2026-05-15 09:32:43 +0200

    feat: move Google AdSense script to head for improved loading performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7185526762692935"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="flex flex-col min-h-screen">
         <Navbar />
         <div className="flex-grow">
@@ -56,12 +63,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <OneSignalInit />
         <SpeedInsights />
         <Analytics />
-        {/* Google AdSense */}
-        <Script
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7185526762692935"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
-        />
         {/* Google Analytics */}
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-JF4KBF34HB" strategy="afterInteractive" />
         <Script id="ga-init" strategy="afterInteractive">{`
